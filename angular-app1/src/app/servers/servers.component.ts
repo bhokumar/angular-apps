745ab1cadd560acea6d4279c2076dff1b2c9987f
Bhoopendra Kumar <bhoopendra.akgec@gmail.com>
2019-03-13 09:57:53 +0530

    Adding Basics of Angular

--- angular-app1/src/app/servers/servers.component.ts
+++ angular-app1/src/app/servers/servers.component.ts
@@ -7,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  serverId: number = 10;
+  status: string='Offline';
+  allowNewServer= true;
+  serverCreationStatus = 'No server is created yet!';
+  serverName='';
+  serverCreated=false;
 
-  constructor() { }
+  constructor() { 
+    this.status = Math.random()>.5 ? 'Online' : 'Offline';
+  }
 
   ngOnInit() {
+  }
+
+  getServerStatus() {
+    return this.status;
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created!';
+  }
+
+  onUpdateServerName(event: Event) {
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  getColor() {
+    return this.status === 'Online' ? 'green' : 'red';
   }
 
 }
